ca07a840c39b8fa6916538f5da85ebe2061c6fa4
gauravjalap <luckyskchirag@gmail.com>
2024-09-22 11:45:40 +0530

    Admin panel Added

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./List.css";
+const List = () => {
+  return <div></div>;
+};
+import "./List.css";
+export default List;
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Orders.css"
+const Orders = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import "./Orders.css"
+export default Orders
