8b68a42a72570a9320d61a6af86b734fce9d3731
gauravjalap <luckyskchirag@gmail.com>
2024-09-24 16:56:35 +0530

    frontend url added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  // const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-delivery-website-frontend.vercel.app/";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
