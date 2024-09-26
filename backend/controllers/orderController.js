bc0947293be05bc7190756b5c7226dd0c9fca9b9
gauravjalap <luckyskchirag@gmail.com>
2024-09-26 21:52:24 +0530

    url updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://feastflies.onrender.com/";
+  // const frontend_url = "https://feastflies.onrender.com/";
+  const frontend_url = "http://localhost:5174";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
